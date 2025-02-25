cfbd3117b02daf67a743e9c2f8e1fb5130741b33
James Lumawag <64976921+Satsuu@users.noreply.github.com>
2025-02-26 06:29:27 +0800

    display api key

--- routifi.frontend/src/components/settings/settings-page.tsx
+++ routifi.frontend/src/components/settings/settings-page.tsx
@@ -4,7 +4,16 @@ import { Breadcrumbs } from "@/components/breadcrumbs";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 
+import { useParams } from "react-router-dom";
+import { useOrganizationStore } from "@/store/organizationStore";
+
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+
 export default function Settings() {
+  const { slug } = useParams<{ slug: string }>();
+  const { organizations } = useOrganizationStore();
+
   return (
     <PageContainer>
       <div className="space-y-4">
@@ -17,6 +26,17 @@ export default function Settings() {
           />
         </div>
         <Separator />
+      </div>
+
+      <div className="mt-4 space-y-4">
+        {organizations
+          .filter((org) => org.slug === slug)
+          .map((org) => (
+            <div key={org.id}>
+              <Label htmlFor="api_key">API KEY</Label>
+              <Input className="p-5" disabled value={org.api_key} />
+            </div>
+          ))}
       </div>
     </PageContainer>
   );
