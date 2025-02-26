f29115e80d3690d733c7effb9337a5716d69f558
James Lumawag <64976921+Satsuu@users.noreply.github.com>
2025-02-26 18:51:42 +0800

    update api key display

--- routifi.frontend/src/components/settings/settings-page.tsx
+++ routifi.frontend/src/components/settings/settings-page.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { Eye, EyeOff, Copy } from "lucide-react";
+
 import PageContainer from "@/components/page-container";
 
 import { Breadcrumbs } from "@/components/breadcrumbs";
@@ -6,13 +9,26 @@ import { Separator } from "@/components/ui/separator";
 
 import { useParams } from "react-router-dom";
 import { useOrganizationStore } from "@/store/organizationStore";
+import { useToast } from "@/hooks/use-toast";
 
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
+import { Button } from "../ui/button";
 
 export default function Settings() {
   const { slug } = useParams<{ slug: string }>();
   const { organizations } = useOrganizationStore();
+  const { toast } = useToast();
+  const [showApiKey, setShowApiKey] = useState(false);
+
+  const handleCopy = (apiKey?: string) => {
+    if (!apiKey) return;
+
+    navigator.clipboard.writeText(apiKey);
+    toast({
+      title: "API Key copied to clipboard",
+    });
+  };
 
   return (
     <PageContainer>
@@ -32,9 +48,38 @@ export default function Settings() {
         {organizations
           .filter((org) => org.slug === slug)
           .map((org) => (
-            <div key={org.id}>
+            <div key={org.id} className="relative">
               <Label htmlFor="api_key">API KEY</Label>
-              <Input className="p-5" disabled value={org.api_key} />
+              <div className="relative">
+                <Input
+                  className="p-5 pr-14"
+                  disabled
+                  type={showApiKey ? "text" : "password"}
+                  value={org.api_key}
+                />
+                <div className="absolute inset-y-0 right-2 flex items-center space-x-2">
+                  <Button
+                    type="button"
+                    variant="ghost"
+                    size="icon"
+                    onClick={() => setShowApiKey(!showApiKey)}
+                  >
+                    {showApiKey ? (
+                      <EyeOff className="h-5 w-5" />
+                    ) : (
+                      <Eye className="h-5 w-5" />
+                    )}
+                  </Button>
+                  <Button
+                    type="button"
+                    variant="ghost"
+                    size="icon"
+                    onClick={() => handleCopy(org.api_key)}
+                  >
+                    <Copy className="h-5 w-5" />
+                  </Button>
+                </div>
+              </div>
             </div>
           ))}
       </div>
